3b609927aaf1bff0cb6b13c4a9cf5dd9e6a41c36
Виктория Аксентий <peta-holding@yandex.ru>
2021-06-01 21:57:39 +0300

    регистрация

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import GetJWTTokenViewSet, SendCodeViewSet, UserViewSet
+from .views import GetJWTTokenViewSet, SendConfirmationCodeViewSet, UserViewSet
 
 v1_router = DefaultRouter()
 v1_router.register('users', UserViewSet)
@@ -11,7 +11,7 @@ urlpatterns = [
     path('v1/', include(v1_router.urls)),
     path(
         'v1/auth/email/',
-        SendCodeViewSet.as_view(),
+        SendConfirmationCodeViewSet.as_view(),
         name='send_confirmation_code'),
     path('v1/auth/token/', GetJWTTokenViewSet.as_view(), name='get_jwt_token'),
 ]
