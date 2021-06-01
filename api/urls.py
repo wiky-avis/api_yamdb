31a301b38c4155719b3cc0fe2939fa3e4794b683
wiky-avis <peta-holding@yandex.ru>
2021-06-01 12:36:10 +0300

    router

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+
+v1_router = DefaultRouter()
+
+
 urlpatterns = [
+    path('v1/', include(v1_router.urls)),
 ]
