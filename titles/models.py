78dae71be2d851b9f6f72ad4100bac65d8eddf38
Arthur Chahalyan <arthur.inf@mail.ru>
2021-06-01 13:26:02 +0300

    Add models: Review, Comment

--- titles/models.py
+++ titles/models.py
@@ -40,8 +40,70 @@ class Title(models.Model):
 
 
 class Review(models.Model):
-    pass
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Отзыв',
+        help_text='Оставьте отзыв'
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
+    )
+    text = models.TextField()
+    score = models.IntegerField(
+        choices=(
+            (1, '1'),
+            (2, '2'),
+            (3, '3'),
+            (4, '4'),
+            (5, '5'),
+            (6, '6'),
+            (7, '7'),
+            (8, '8'),
+            (9, '9'),
+            (10, '10'),
+        )
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title_id', 'author'],
+                name='already_review',
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:15]
 
 
 class Comment(models.Model):
-    pass
+    review_id = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Комментарий',
+        help_text='Напишите комментарий'
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
+    )
+    text = models.TextField()
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[:15]
