fa7d2b6b29019263a9c40e7ef0317ef994ecf458
wiky-avis <peta-holding@yandex.ru>
2021-06-02 14:58:38 +0300

    permissions 2

--- api/permissions.py
+++ api/permissions.py
@@ -2,12 +2,14 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsAdmin(BasePermission):
+
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return request.user.is_staff or request.user.role == 'admin'
 
 
 class IsAuthorOrAdminOrModerator(BasePermission):
+
     def has_object_permission(self, request, view, obj):
         if request.user and request.user.is_authenticated:
             if (
@@ -18,12 +20,12 @@ class IsAuthorOrAdminOrModerator(BasePermission):
                 request.method == 'POST' and
                 request.user.is_authenticated):
                 return True
-
         elif request.method in SAFE_METHODS:
             return True
 
 
 class IsAdminOrReadOnly(BasePermission):
+
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
