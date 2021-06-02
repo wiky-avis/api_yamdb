95076e433f5ad15ac498fcb4b39c5ac0f5d4b3ef
wiky-avis <peta-holding@yandex.ru>
2021-06-02 17:09:54 +0300

    редактирование профиля users/me

--- api/views.py
+++ api/views.py
@@ -2,7 +2,11 @@ import secrets
 
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
-from rest_framework import generics, viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .permissions import IsAdmin, IsAdminOrReadOnly, IsAuthorOrAdminOrModerator
@@ -38,3 +42,19 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsAdmin]
+
+    @action(
+        methods=['GET', 'PATCH'],
+        detail=False,
+        permission_classes=[IsAuthenticated])
+    def me(self, request, pk=None):
+        user = get_object_or_404(User, email=self.request.user.email)
+        if request.method == 'PATCH':
+            serializer = UserSerializer(user, data=request.data, partial=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = UserSerializer(self.request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
