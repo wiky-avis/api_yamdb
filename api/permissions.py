aebfdb32f479d0792ed5a0514285e5a56098276b
wiky-avis <peta-holding@yandex.ru>
2021-06-03 16:15:49 +0300

    update permission IsAuthorOrAdminOrModerator

--- api/permissions.py
+++ api/permissions.py
@@ -9,6 +9,11 @@ class IsAdmin(BasePermission):
 
 
 class IsAuthorOrAdminOrModerator(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         if request.user and request.user.is_authenticated:
